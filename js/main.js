732fc876f42f01b47bb39d37918abc86f0f4578b
Ruslan Akseonov <54501083+akseonowww@users.noreply.github.com>
2022-06-14 05:36:15 +0300

    Fix

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-'use strict';
-
 function toName() {
    const avatarMini = document.querySelector('.avatar-mini'),
       namePeople = document.querySelector('.avatar-mini .ofther-people__name');
@@ -11,3 +9,19 @@ function toName() {
 }
 
 toName();
+
+const openCardProject = (el) => {
+   element = document.querySelector(".project" + el)
+   element.classList.add('active-project')
+   document.getElementsByTagName("body")[0].style = "overflow: hidden"
+   console.log(element.classList)
+}
+
+const closeCardProject = (el) => {
+   element = document.querySelector(".project" + el)
+   element.classList.remove('active-project')
+   document.getElementsByTagName("body")[0].style = "overflow-y: scroll"
+   console.log(element.classList)
+}
+
+
